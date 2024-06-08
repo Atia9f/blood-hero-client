1a6ad570e15cce2b6d177210fbfbbed10b7b7c1f
Atia9f <atiafairujakhi2003@gmail.com>
2024-06-08 14:54:05 +0600

    hide key

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -6,12 +6,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAbwCXDb5H5U88rcpea84BtFDplUNmMsJE",
-  authDomain: "assignment-12-58f27.firebaseapp.com",
-  projectId: "assignment-12-58f27",
-  storageBucket: "assignment-12-58f27.appspot.com",
-  messagingSenderId: "103510832466",
-  appId: "1:103510832466:web:da0d92980e06a44a40b68b"
+  apiKey: import.meta.env.VITE_apiKey ,
+  authDomain: import.meta.env.VITE_authDomain ,
+  projectId: import.meta.env.VITE_projectId ,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId ,
+  appId: import.meta.env.VITE_appId 
 };
 
 // Initialize Firebase
